0c8907217b88d3000c3eeeae6ca501b718fcde55
Aijá Gourgel <agourgel44@gmail.com>
2026-05-18 10:10:39 +0100

    Removendo Override de userRepository

--- crudApp/src/main/java/com/grupoSI/crudApp/database/repository/UserRepository.java
+++ crudApp/src/main/java/com/grupoSI/crudApp/database/repository/UserRepository.java
@@ -1,13 +1,13 @@
 package com.grupoSI.crudApp.database.repository;
 
-import com.grupoSI.crudApp.database.model.User;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import com.grupoSI.crudApp.database.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    @Override
     Optional<User> findByEmail(String email);
 }
